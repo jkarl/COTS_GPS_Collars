9af2fdb7c26eaafd20ce3fbbfac8e049d7b55b1b
shry9735 <43352820+shry9735@users.noreply.github.com>
2019-04-04 21:22:05 -0700

    Update NewCollar
    Made the program readable, grouped lines of code.  Added systeminit function.  Added blink function. Added sleep function.  None of these should affect behavior at all, and should be able to be uploaded right away.  Minor tweaks should come next, followed by Functionality updates.

--- firmware/MSR1-R3/NewCollar/NewCollar.ino
+++ firmware/MSR1-R3/NewCollar/NewCollar.ino
@@ -1,6 +1,4 @@
 #include <NMEAGPS.h>
-NMEAGPS GPS;
-gps_fix fix;
 #include <SD.h>
 
 #define LED_PIN (5)
@@ -8,85 +6,85 @@ gps_fix fix;
 #define TXPin (3)
 #define LED1 (5)
 #define LED2 (6)
-#define GPSPOWER (7)
+#define GPSpower (7)
 #define SDCHIPSELECT (8)
 #define GREENLED (LED1)
 #define REDLED (LED2)
 #include <NeoSWSerial.h>
 #define ARDUINO_GPS_RX 3 // GPS TX, Arduino RX pin
 #define ARDUINO_GPS_TX 4 // GPS RX, Arduino TX pin
-NeoSWSerial gpsPort(ARDUINO_GPS_TX, ARDUINO_GPS_RX);
-//#define gpsPort Serial  // Alternatively, use Serial1 on the Leonardo, Mega or Due
 #define GPS_BAUD 9600    // GPS module baud rate. GP3906 defaults to 9600.
+#define Second 1000
+
+
+NMEAGPS GPS;
+gps_fix fix;
+NeoSWSerial gpsPort(ARDUINO_GPS_TX, ARDUINO_GPS_RX);
 
       bool          waitingForFix = true;
 const unsigned long GPS_TIMEOUT   = 60000; // 2 minutes
-      unsigned long gpsStart      = 0;
-
-const int           GPSpower      = 7;
-bool turnGPSoff = false;
-File dataFile;
+      unsigned long GPS_TIME      = 0;
+      bool turnGPSoff = false;
+      File dataFile;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Starting....");
-  pinMode(GPSpower, OUTPUT);
+  SystemInitialize();
   digitalWrite(GPSpower,HIGH);
   gpsPort.begin(GPS_BAUD);
-  pinMode(REDLED,OUTPUT);
-  pinMode(GREENLED,OUTPUT);
-  if (!SD.begin(SDCHIPSELECT)) // see if the card is present and can be initialized, also sets the object to hold onto that chip select pin
-  {  
-    //Serial.println("SD fail"); //Tell PC, can be commented out
-    //Serial.println("Halting...");
-    digitalWrite(GREENLED,LOW); 
-    digitalWrite(REDLED,HIGH); 
-    SD.end();
-    while(1);
-  }
+
+  
   
 }
 
 
 void loop() {
-  digitalWrite(GREENLED,!digitalRead(GREENLED));
-  // Is a GPS fix available?
-  if (GPS.available( gpsPort )) {
+  digitalWrite(GREENLED,!digitalRead(GREENLED)); //heartbeat
+  
+  // Is a GPS fix available?*******************
+  if (GPS.available( gpsPort )) 
+  {
     fix = GPS.read();
-
-    if (waitingForFix) {
-      printGPSInfo();
-      Serial.println( millis() - gpsStart ); // DEBUG
-
-      //if (fix.valid.satellites && (fix.satellites > 6)) {// I dont trust this
-      //  waitingForFix = false;
-      //  turnGPSoff    = true;
-      //}
+    if (waitingForFix) 
+    {
+      if(printGPSInfo())//Attempt to print
+        {
+          waitingForFix = false;
+          turnGPSoff    = true;
+        }
+      Serial.println( millis() - GPS_TIME ); // DEBUG
     }
   }
+  //***************************************
 
+  
   // Have we waited too long for a GPS fix?
  // if (waitingForFix && (millis() - gpsStart > GPS_TIMEOUT)) {
  //   waitingForFix = false;
  //   turnGPSoff    = true;
   //}
-
-  if (turnGPSoff) {
+//******************************************
+ 
+ //Sleep 
+  if (turnGPSoff) 
+  {
     digitalWrite(GPSpower, LOW);
     digitalWrite(GREENLED,LOW);
     digitalWrite(REDLED,HIGH);
-    Serial.println( F("GPS Toggled") );
-    delay(1000*30);
+    Sleep(1);
     digitalWrite(REDLED,LOW);
     digitalWrite(GPSpower,HIGH);
     waitingForFix = true;
     turnGPSoff    = false;
+    delay(15*Second); 
   }
+  //******************************
 
 } // loop
 
 
-void printGPSInfo()
+bool printGPSInfo()
 {
   if (fix.valid.location){
   if (fix.valid.altitude){
@@ -99,6 +97,7 @@ void printGPSInfo()
     Serial.println(fix.dateTime.hours);
     dataFile = SD.open("gpslog.csv", FILE_WRITE); //open SD
     if (dataFile)
+    {
     dataFile.print(String(fix.dateTime.year)  +",");
     dataFile.print(String(fix.dateTime.month)+",");
     dataFile.print(String(fix.dateTime.date)+",");
@@ -113,7 +112,50 @@ void printGPSInfo()
     dataFile.print(fix.latitude(),10);dataFile.print(",");
     dataFile.println(fix.longitude(),10);
     dataFile.close();
-    waitingForFix = false;
-    turnGPSoff    = true;
+    return true;
+    }
+    else
+    {
+      Blink(REDLED);
+      Blink(REDLED);
+    }
   }}}}}}}}
+  return false;
+}
+
+void Sleep(int Minutes)
+{
+  delay(Second*60*Minutes);
+}
+
+void Blink(int pin)
+{
+  digitalWrite(pin,LOW);
+  delay(10);
+  digitalWrite(pin,HIGH);
+  delay(200);
+  digitalWrite(pin,LOW);
+  delay(200);
+  digitalWrite(pin,HIGH); 
+  delay(200);
+  digitalWrite(pin,LOW);
+}
+
+void SystemInitialize()
+{
+  pinMode(GPSpower, OUTPUT);
+  pinMode(REDLED,OUTPUT);
+  pinMode(GREENLED,OUTPUT);
+  if (!SD.begin(SDCHIPSELECT)) // see if the card is present and can be initialized, also sets the object to hold onto that chip select pin
+  {  
+    //Serial.println("SD fail"); //Tell PC, can be commented out
+    //Serial.println("Halting...");
+    digitalWrite(GREENLED,LOW); 
+    digitalWrite(REDLED,HIGH); 
+    SD.end();
+    while(1)
+    {
+      Blink(REDLED);
+    }
+  }
 }
